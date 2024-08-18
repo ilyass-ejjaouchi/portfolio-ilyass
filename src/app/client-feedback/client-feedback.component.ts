f592bde75a2dc6877075483954e837f68e54ee15
ilyass-ejjaouchi <ilyass-ejjaouchi@users.noreply.github.com>
2024-08-18 17:11:04 +0000

    deploy: 25c1eeaf9a30174a2b44d3dfd41c84e7714030ab

---
+++ src/app/client-feedback/client-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-feedback.component.html',
+  styleUrl: './client-feedback.component.css'
+})
+export class ClientFeedbackComponent {
+
+}
