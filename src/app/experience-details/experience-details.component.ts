18dc85154b3c8636e92c2a46536651c35d6dae8c
ilyassejjaouchi <ilyass.ejjaouchi@gmail.com>
2024-08-19 17:42:36 +0200

    make the navbar responsive

--- src/app/experience-details/experience-details.component.ts
+++ src/app/experience-details/experience-details.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {CommonModule} from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Location } from '@angular/common';
 
 @Component({
@@ -10,7 +10,7 @@ import { Location } from '@angular/common';
   templateUrl: './experience-details.component.html',
   styleUrl: './experience-details.component.css'
 })
-export class ExperienceDetailsComponent implements OnInit{
+export class ExperienceDetailsComponent implements OnInit {
   projectTitle: string = '';
   company: string = '';
   location: string = '';
@@ -26,50 +26,61 @@ export class ExperienceDetailsComponent implements OnInit{
 
       // Based on projectId, set the details including images
       if (projectId === 'sncf') {
-        this.projectTitle = 'SNCF Ticket Management';
+        this.projectTitle = 'Development of a Reservation and Ticketing Management Application for SNCF Stations';
         this.company = 'CAPGEMINI';
         this.location = 'Nantes, France';
         this.duration = '08/2022 - Present';
         this.projectDetails = [
-          'Technical analysis of existing systems, extraction of business rules.',
-          'Modeling of APIs and drafting of technical documentation and developer guides.',
-          'Design of interface contracts linking the application to other SNCF applications.',
-          'Implementation of a microservices architecture using Spring Cloud, Java 21, PostgreSQL.',
-          'Managed the migration from Spring to Spring Boot and updated Java to version 17 to optimize performance.',
-          'Managed the redesign of the architecture using Spring Boot, while improving exception handling.',
-          'Use of SONAR and Jenkins tools to improve code quality, while reducing technical debt.',
-          'Efficient coordination of development efforts between back-end and front-end teams, facilitating communication with POs (Product Owners) and the certification team.',
-          'Technical mentoring provided to team members, support during migrations, creation of detailed documentation.'
+          'Precise migration from Spring to Spring Boot and upgrading to Java 17 were executed, unlocking innovative functionalities that enhanced code performance and maintenance, optimizing existing workflows.',
+          'Systematic architecture redesign was accomplished by integrating Spring Boot, with a keen focus on optimizing exception handling, boosting application reliability.',
+          'Strategic use of SONAR and Jenkins fostered initiatives to elevate code quality, playing a crucial role in identifying and mitigating technical debt, fostering a more structured and reliable coding environment.',
+          'Effective coordination was established between back-end and front-end teams, nurturing enriching cross-team collaboration. This enhanced communication with Product Owners and the testing team, ensuring synchronized and clear understanding of goals and requirements.',
+          'In-depth mentoring and technical support facilitated migration phases. Crafting comprehensive and detailed documentations was prioritized, contributing to an enriched learning and development environment.'
         ];
         this.projectImage = 'assets/sncf.png';  // Set the image path
       } else if (projectId === 'nuclear') {
-        this.projectTitle = 'Nuclear Plant Maintenance';
+        this.projectTitle = 'Development of an Application for Planning and Executing Maintenance Activities of Nuclear Power Plants';
         this.company = 'CGI';
         this.location = 'Lyon, France';
         this.duration = '02/2022 - 07/2022';
         this.projectDetails = [
-          'Incident management with Spring Boot and Angular, coordination via GitLab.',
-          'Talend job design and optimization of Oracle and PostgreSQL databases with PL/SQL scripts for data administration.',
-          'Technical design and development of solutions with Spring Boot, Angular, and Bootstrap.'
+          'Analyzed and resolved various application incidents and problems, ensuring service continuity and user satisfaction.',
+          'Developed and executed unit tests for new functionalities to validate quality and performance.',
+          'Enhanced data administration using PL/SQL scripts, enabling more efficient and reliable data management.',
+          'Crafted Talend jobs to automate and optimize various processes, improving workflow management.',
+          'Technically designed the application, ensuring robust, reliable solutions that meet project needs.'
         ];
         this.projectImage = 'assets/edf.png';  // Set the image path
       } else if (projectId === 'transfer') {
-        this.projectTitle = 'International Transfer Management';
+        this.projectTitle = 'Digital Banking Solution Back End in Microservices & Front End with React';
         this.company = 'Adria Business & Technology';
-        this.location = 'Casablanca, Maroc';
+        this.location = 'Casablanca, Morocco';
         this.duration = '02/2021 - 09/2021';
         this.projectDetails = [
-          'Design of an application integrating Spring Boot, React JS, and other tools to optimize the management of international transfers.',
-          'International transfer request and beneficiary registration.',
-          'Request signature and history consultation.',
-          'Development of a secure backend module with Spring Boot to reinforce the confidentiality and integrity of business data.',
-          'Encrypting and decrypting downloaded files during order delivery.'
+          'Development of a module for securing downloaded files, ensuring robust integrity and confidentiality during transmission.',
+          'Implementation of an advanced decryption functionality, guaranteeing thorough validity checks of files before further manipulation.',
+          'Introduction of an innovative file sealing system, using a distinct public key for each client, enhancing the security and authenticity of shared data.',
+          'Configuration of a standardized encryption method, using a unique key for all clients, optimizing the decryption process and file accessibility.',
+          'Design and deployment of a comprehensive microservices application, facilitating various essential operations related to international transfers.',
+          'Development of a feature for easy declaration of international beneficiaries, increasing transaction transparency and traceability.'
         ];
         this.projectImage = 'assets/abs.png';  // Set the image path
+      } else if (projectId === 'mobileApp') {
+        this.projectTitle = 'Design and Implementation of a Mobile Application for Construction Site Management';
+        this.company = 'ABC Construction';
+        this.location = 'Paris, France';
+        this.duration = '03/2020 - 01/2021';
+        this.projectDetails = [
+          'Significant contribution to the design of a robust mobile application, with the creation of a well-structured database using MySQL and Hibernate.',
+          'Implementation of a strong and secure authentication system using JWT and Spring Security.',
+          'Utilized Spring Boot, Angular, and Ionic for effective development, with successful cloud deployment via Heroku.'
+        ];
+        this.projectImage = 'assets/abc_construction.png';  // Set the image path
       }
-      // Add more cases for other projects
+      // Add more cases for other projects as needed
     });
   }
+
   goBack(): void {
     this._location.back();
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {NgClass, NgOptimizedImage, ViewportScroller} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgClass,
+    RouterLink,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  constructor(private viewportScroller: ViewportScroller) {
+  }
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollToSection(sectionId: string) {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
