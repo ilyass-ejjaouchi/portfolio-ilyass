09a2fbd5a9fd2ac43bb93948a56a6336bac9b215
ilyassejjaouchi <ilyass.ejjaouchi@gmail.com>
2024-08-19 17:49:08 +0200

    keep the navbar even we scroll

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import {NgClass, NgOptimizedImage, ViewportScroller} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import { Component, HostListener } from '@angular/core';
+import { NgClass, NgOptimizedImage, ViewportScroller } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -15,14 +15,34 @@ import {RouterLink} from "@angular/router";
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  currentSection = 'intro';
 
-  constructor(private viewportScroller: ViewportScroller) {
-  }
+  constructor(private viewportScroller: ViewportScroller) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
   scrollToSection(sectionId: string) {
+    this.currentSection = sectionId;
     this.viewportScroller.scrollToAnchor(sectionId);
+    this.toggleMenu();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = ['intro', 'portfolio', 'about-me', 'client-feedback'];
+    let current = sections[0];
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+          current = section;
+          break;
+        }
+      }
+    }
+    this.currentSection = current;
   }
 }
