a4e6d5256496a4b6645b9b16d304b821bbfd4578
ilyass-ejjaouchi <ilyass-ejjaouchi@users.noreply.github.com>
2024-08-19 09:23:14 +0000

    deploy: 0fa6ef9d8ab61c8b89706984b7b496964444212d

--- src/app/expertise/expertise.component.ts
+++ src/app/expertise/expertise.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-expertise',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './expertise.component.html',
   styleUrl: './expertise.component.css'
 })
